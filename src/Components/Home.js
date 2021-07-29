770270273f6f5d0470a6a7e4a54cef528dd30c08
Suraj Adav <suraj.adav20@gmail.com>
2021-07-29 21:21:56 +0530

    added searchbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,23 +4,25 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumbnail from './Thumbnail';
+import SearchBar from './SearchBar';
 // import ImgNotFound from '../images/#.jpg'
 
 import { useHomeFetch } from '../hooks/useHomeFetch';
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
   console.log(state);
   return (
     <>
-      {state.results[0] ? (
+      {!searchTerm && state.results[0] ? (
         <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
           title={state.results[0].original_title}
           text={state.results[0].overview}
         />
       ) : null}
-      <Grid header='Popular'>
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <Grid header={searchTerm ? 'Search Results' : 'Popular'}>
         {state.results.map((movie) => (
           <Thumbnail
             key={movie.id}
